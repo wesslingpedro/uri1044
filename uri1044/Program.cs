4f2bcfde7809bca94a63ec63754dac67ad610d7d
wesslingpedro <wesslingpedro@gmail.com>
2024-05-21 19:07:36 -0300

    Primeiro commit

---
+++ uri1044/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+
+namespace uri1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int A = int.Parse(valores[0]);
+            int B = int.Parse(valores[1]);
+
+         if (A % B == 0 || B % A == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+         else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+
+            
+
+
+
+        }
+
+    }
+
+}
